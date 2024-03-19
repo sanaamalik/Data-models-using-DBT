f1163bb870993e8650164cb99dde4d72f7723296
Ahmednizami <muhammadahmedsaeednizami@gmail.com>
2024-03-19 16:21:11 +0500

    Expense Table Update

--- models/expense.sql
+++ models/expense.sql
@@ -3,7 +3,7 @@
 SELECT 
     e.job_id,
     e.amount,
-    e.item_name
+    e.item_name as type
 FROM {{ ref('account') }} a
 INNER JOIN {{ ref('expenses') }} e on e.item_value = a.id 
 order by job_id
