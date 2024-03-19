e8bdb7d9b296e216c0a5b70afabe872d0cd29097
Ahmednizami <muhammadahmedsaeednizami@gmail.com>
2024-03-19 16:03:05 +0500

    Invoice Table

--- models/invoice.sql
+++ models/invoice.sql
@@ -3,8 +3,8 @@
 SELECT 
     i.job_id,
     i.amount,
-    it.incomename,
-    i.detail_type
+    it.incomename AS item,
+    i.detail_type AS type
 FROM {{ ref('item') }} it
 right join {{ ref('invoices') }} i on i.item_value = it.id
 order by job_id
