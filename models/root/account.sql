a11fb1959ae447699695cc9b06d0939c4daa653d
Ahmednizami <muhammadahmedsaeednizami@gmail.com>
2024-03-19 14:07:54 +0500

    Accounts Table Logic

--- models/root/account.sql
+++ models/root/account.sql
@@ -1,6 +1,7 @@
 {{ config(materialized='table') }}
 
 SELECT 
+    "Id" as id,
     "Name" as name,
     CASE 
         WHEN "ParentRef" IS NOT NULL THEN "ParentRef"->>'value'
@@ -11,4 +12,5 @@ SELECT
     "Classification" as classification,
     "FullyQualifiedName" as subfield
 FROM public."qb_full_accounts"
+ORDER BY "Id"::integer
     
